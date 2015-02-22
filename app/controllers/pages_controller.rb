4ea5a612b0c505880db944483c6e99c244a100de
Jesse Furmanek <jesse.furmanek@gmail.com>
2015-02-22 22:57:12 +0000

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  
+  before_action :require_user, except: [:contact, :help]
+  
+  def signup
+  end
+
+  def contact
+  end
+
+  def help
+  end
+
+  def about
+  end
+end
